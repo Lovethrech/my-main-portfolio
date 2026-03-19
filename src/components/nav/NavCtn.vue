aaedc2651a8cb4995e27e3f08b22f039e38a5ada
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 18:36:02 +0100

    working on the header components and possible sub files.

--- src/components/nav/NavCtn.vue
+++ src/components/nav/NavCtn.vue
@@ -1,5 +1,11 @@
+<script setup>
+import DesktopNav from './DesktopNav.vue';
+</script>
+
 <template>
     <section class="nav-ctn">
-
+        <div class="nav-ctn-main-ctn">
+            <DesktopNav/>
+        </div>
     </section>
 </template>
