1b780e1e9f5936dd15b0ef3f3b9edf7a87526cd7
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 21:24:22 +0100

    working on the header components and possible sub files.

--- src/components/nav/MobileNav.vue
+++ src/components/nav/MobileNav.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="mobile-nav">
-        
+        <div class="mobile-nav-main-ctn">
+            <div class="mobile-nav-menu-icon">
+                
+            </div>
+            <div class="mobile-nav-menu-dropdown">
+
+            </div>
+        </div>
     </div>
 </template>
