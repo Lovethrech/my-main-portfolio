5ab99f81b1ba32ea7ced11d93a015dcd6000ba68
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 18:02:35 +0100

    starting with the header components and possible sub files.

--- src/components/header/HeaderLogo.vue
+++ src/components/header/HeaderLogo.vue
@@ -2,4 +2,15 @@
     <div class="header-logo">
         <img src="/public/ChelModernLogo.svg" alt="">
     </div>
+    <!-- #6b7280; , #334155;-->
 </template>
+
+<style scoped>
+.header-logo{
+    background-color: #334155;
+}
+.header-logo img{
+    width: 100%;
+    height: 1005;
+}
+</style>
