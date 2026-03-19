87258ef20f4290a9a74b0c9a3833913d7c6d8529
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 18:38:09 +0100

    working on the header components and possible sub files.

--- src/components/nav/DesktopNav.vue
+++ src/components/nav/DesktopNav.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="desktop-nav">
-        
+        <div class="desktop-nav-main-ctn">
+            <RouterLink to="">
+                <p class="desktop-nav-link-desc">
+                    Home
+                </p>
+            </RouterLink>
+        </div>
     </div>
 </template>
