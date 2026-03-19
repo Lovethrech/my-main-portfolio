65f742cc6a132eecdaf31efa94fb943c9a10ace8
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 18:56:45 +0100

    working on the header components and possible sub files.

--- src/components/hero/HeroCtn.vue
+++ src/components/hero/HeroCtn.vue
@@ -3,3 +3,10 @@
         
     </section>
 </template>
+
+<style scoped>
+.hero-ctn{
+    min-height: 40vh;
+    background-color: var(--general-components-bg-color);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup>
 import MainHeader from '@/components/MainHeader.vue';
+import HeroCtn from '@/components/hero/HeroCtn.vue';
 </script>
 
 <template>
   <MainHeader/>
+  <main>
+    <HeroCtn/>
+  </main>
 </template>
