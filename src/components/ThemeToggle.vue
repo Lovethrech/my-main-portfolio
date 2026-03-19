4daf9d86cad51440e00da578853b662991f73ee1
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 17:06:51 +0100

    ...updating the ThemeToggle components for theme implementation.

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { useThemeStore } from '@/stores/theme';
 import { storeToRefs } from 'pinia';
+
+const themeStore=useThemeStore();
+const {isDark}=storeToRefs(themeStore);
 </script>
 
 <template>
     <button
-        @click="useThemeStore.toggleTheme" 
+        @click="themeStore.toggleTheme" 
         class="theme-toggle"
     >
         {{ isDark? 'Light Mode' : 'Dark Mode' }}
