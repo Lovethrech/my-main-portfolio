1336935fce2f1bf83c4de5919403d19a7cb94c4f
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 17:44:06 +0100

    starting with the header components and possible sub files.

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,9 @@
+<script setup>
+import HeaderLogo from './header/HeaderLogo.vue';
+</script>
+
 <template>
     <header class="main-header">
-        
+
     </header>
 </template>
--- src/components/header/HeaderLogo.vue
+++ src/components/header/HeaderLogo.vue
@@ -1,3 +1,5 @@
 <template>
-    
+    <div class="header-logo">
+        <img src="/public/ChelModernLogo.svg" alt="">
+    </div>
 </template>
