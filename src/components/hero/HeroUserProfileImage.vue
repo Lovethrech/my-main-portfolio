c874a801df21d19a4a88bb6eb4dd8fae1a7c36bb
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 23:46:43 +0100

    working on the hero components.

--- src/components/hero/HeroUserProfileImage.vue
+++ src/components/hero/HeroUserProfileImage.vue
@@ -14,14 +14,7 @@
 
 <style scoped>
 .hero-user-profile-image-ctn{
-    background-color: var(--general-components-bg-color);
-    opacity: 0.8;
-    background-size: 20px 20px;
-    background-image:  repeating-linear-gradient(to right, 
-        var(--bg-color), var(--bg-color) 1px, 
-        var(--general-components-bg-color) 1px,
-        var(--general-components-bg-color))
-    ;
+    background: url("/public/wavy.svg") bottom/ auto no-repeat;
 }
 .hero-user-profile-image-ctn img {
     width: 100%;
