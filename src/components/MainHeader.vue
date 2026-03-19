476aed1c5ed1cbb8e1ad0b08808e88009d20ef4d
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 18:44:42 +0100

    working on the header components and possible sub files.

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -14,5 +14,8 @@ import NavCtn from './nav/NavCtn.vue';
 .main-header{
     width: 100%;
     background-color: rgba(146, 107, 107, 0.411);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 }
 </style>
