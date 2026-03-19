744fce837f9aaea8821367480566aaf9e78352b2
Lovethrech <rachealdolapo45@gmail.com>
2026-03-19 23:42:44 +0100

    working on the hero components.

--- src/components/hero/HeroUserProfileImage.vue
+++ src/components/hero/HeroUserProfileImage.vue
@@ -1,16 +1,27 @@
 <template>
     <div class="hero-user-profile-image-ctn">
         <img src="/public/userProfile.png" alt="Lovethrech Image">
-        <!-- background-color: #e5e5f7;
-opacity: 0.8;
-background-size: 20px 20px;
-background-image:  repeating-linear-gradient(to right, #444cf7, #444cf7 1px, #e5e5f7 1px, #e5e5f7); -->
+        <!-- background-color: var(--general-components-bg-color);
+    opacity: 0.8;
+    background-size: 20px 20px;
+    background-image:  repeating-linear-gradient(to right, 
+        var(--bg-color), var(--bg-color) 1px, 
+        var(--general-components-bg-color) 1px,
+        var(--general-components-bg-color))
+    ; -->
     </div>
 </template>
 
 <style scoped>
 .hero-user-profile-image-ctn{
-    background-color: orange;
+    background-color: var(--general-components-bg-color);
+    opacity: 0.8;
+    background-size: 20px 20px;
+    background-image:  repeating-linear-gradient(to right, 
+        var(--bg-color), var(--bg-color) 1px, 
+        var(--general-components-bg-color) 1px,
+        var(--general-components-bg-color))
+    ;
 }
 .hero-user-profile-image-ctn img {
     width: 100%;
