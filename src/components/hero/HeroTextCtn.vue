71a867369f43e29c90da1a41231e9a08d10fcfc1
Lovethrech <rachealdolapo45@gmail.com>
2026-03-20 22:31:58 +0100

    working on the hero components.

--- src/components/hero/HeroTextCtn.vue
+++ src/components/hero/HeroTextCtn.vue
@@ -52,7 +52,7 @@ const boxDefaultWidth=ref("40vh");
     transition: box-shadow 0.4s ease, transform 0.3s ease;
 }
 .hero-text-author-job-role{
-    font-size: 6vh;
+    font-size: 10vh;
     text-transform: uppercase;
 }
 </style>
