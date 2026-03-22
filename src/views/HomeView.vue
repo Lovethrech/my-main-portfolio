1d5e5e3fef75ebca1ac2fb3d9fdf010a95f94657
Lovethrech <rachealdolapo45@gmail.com>
2026-03-23 00:52:11 +0100

    working on the hero components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HomeHeroCtn from '@/components/home/hero/HomeHeroCtn.vue';
+import HomeAboutCtn from '@/components/home/about/HomeAboutCtn.vue';
 </script>
 
 <template>
   <main id="home-view">
     <HomeHeroCtn/>
+    <HomeAboutCtn/>
   </main>
 </template>
