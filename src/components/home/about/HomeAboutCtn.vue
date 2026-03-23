83741f12b0144209e26776144a9700fe0bea0b42
Lovethrech <rachealdolapo45@gmail.com>
2026-03-23 01:02:39 +0100

    working on the home-about components.

--- src/components/home/about/HomeAboutCtn.vue
+++ src/components/home/about/HomeAboutCtn.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HomeAboutTitle from './HomeAboutTitle.vue';
+import HomeAboutDesc from './HomeAboutDesc.vue';
 </script>
 
 <template>
     <section class="home-about-ctn">
         <div class="home-about-main-ctn">
             <HomeAboutTitle/>
+            <HomeAboutDesc/>
         </div>
     </section>
 </template>
