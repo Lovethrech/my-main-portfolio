8e8f21a4e824ef066bf5eb0bd64410060e73fcfb
Lovethrech <rachealdolapo45@gmail.com>
2026-03-23 01:03:58 +0100

    working on the home-about components.

--- src/components/home/about/HomeAboutCtn.vue
+++ src/components/home/about/HomeAboutCtn.vue
@@ -4,7 +4,7 @@ import HomeAboutDesc from './HomeAboutDesc.vue';
 </script>
 
 <template>
-    <section class="home-about-ctn">
+    <section class="home-about-ctn display-flex-justify-center-place-center">
         <div class="home-about-main-ctn">
             <HomeAboutTitle/>
             <HomeAboutDesc/>
@@ -14,6 +14,7 @@ import HomeAboutDesc from './HomeAboutDesc.vue';
 
 <style scoped>
 .home-about-ctn{
+    min-height:100vh;
     padding: 3vh 8vh;
 }
 </style>
