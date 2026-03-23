0085c05630ec81271bf6ff4cd6c8501734f091dd
Lovethrech <rachealdolapo45@gmail.com>
2026-03-23 19:45:26 +0100

    working on the home-about components.

--- src/components/home/about/HomeAboutDescStats.vue
+++ src/components/home/about/HomeAboutDescStats.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="home-about-desc-stats">
-        
+        <p id="home-about-desc-stats-client-xperience">
+            3+ Clients Projects
+        </p>
+        <p id="home-about-desc-stats-personal-xperience">
+            10+ Personal Projects
+        </p>
+        <p id="home-about-desc-stats-ongoing-pro">
+            4+ Ongoing Projects
+        </p>
     </div>
 </template>
