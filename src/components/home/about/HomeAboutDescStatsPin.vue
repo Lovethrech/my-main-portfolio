857ddc1f0763684a9ae3ff511e4f0f1a226bc47d
Lovethrech <rachealdolapo45@gmail.com>
2026-03-24 22:49:15 +0100

    working on the home-about components.

--- src/components/home/about/HomeAboutDescStatsPin.vue
+++ src/components/home/about/HomeAboutDescStatsPin.vue
@@ -3,5 +3,18 @@
         <div id="home-about-desc-stats-pin-ctn">
             <i class="ri-pushpin-fill"></i>
         </div>
+        <!-- <i class="ri-github-line"></i> -->
     </div>
 </template>
+
+<style scoped>
+#home-about-desc-stats-pin-ctn{
+    background-color: red;
+}
+.ri-pushpin-fill{
+    font-size: 3vh;
+    background: linear-gradient(to top, #B99A45, #C7AE6A);
+    background-clip: text;
+    color:transparent;
+}
+</style>
