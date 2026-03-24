01e66b5b2da485561d7d8339980cf45de4ac0701
Lovethrech <rachealdolapo45@gmail.com>
2026-03-24 19:53:17 +0100

    working on the home-about components.

--- src/components/home/about/HomeAboutCtn.vue
+++ src/components/home/about/HomeAboutCtn.vue
@@ -2,6 +2,7 @@
 import HomeAboutTitle from './HomeAboutTitle.vue';
 import HomeAboutDesc from './HomeAboutDesc.vue';
 import HomeAboutDescStats from "./HomeAboutDescStats.vue";
+import ColoredBtn from '../ColoredBtn.vue';
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import HomeAboutDescStats from "./HomeAboutDescStats.vue";
                 <HomeAboutDesc/>
                 <HomeAboutDescStats/>
             </div>
+            <ColoredBtn/>
         </div>
     </section>
 </template>
