70021a34538301d50af859bd8b898b4e0b7a4724
Lovethrech <rachealdolapo45@gmail.com>
2026-03-25 13:42:14 +0100

    working on the home-about components.

--- src/components/home/about/HomeAboutDescStatsGit.vue
+++ src/components/home/about/HomeAboutDescStatsGit.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="home-about-desc-stats-git">
-        <a href="https://github.com/Lovethrech" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Lovethrech" target="_blank" rel="noopener noreferrer" class="link">
+            <p class="home-about-desc-stats-git-name">
+                
+            </p>
             Explore
         </a>
     </div>
