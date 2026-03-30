43d4675a9273d3e247965ccb9640b0e3a1c409b9
Lovethrech <rachealdolapo45@gmail.com>
2026-03-30 10:45:23 +0100

    working on the home-about components.

--- src/components/home/project/HomeProjectCtn.vue
+++ src/components/home/project/HomeProjectCtn.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import HomeProjectTitle from './HomeProjectTitle.vue';
 </script>
 
 <template>
     <section class="home-project-ctn">
-
+        <HomeProjectTitle/>
     </section>
 </template>
