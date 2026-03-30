50e566f817d49be8122ad5bb0ac30bf400cf8528
Lovethrech <rachealdolapo45@gmail.com>
2026-03-30 10:44:42 +0100

    working on the home-about components.

--- src/components/home/project/HomeProjectCtn.vue
+++ src/components/home/project/HomeProjectCtn.vue
@@ -1,5 +1,9 @@
+<script setup>
+
+</script>
+
 <template>
     <section class="home-project-ctn">
-        
+
     </section>
 </template>
--- src/components/home/project/HomeProjectTitle.vue
+++ src/components/home/project/HomeProjectTitle.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="home-project-title-ctn">
         <h3 class="home-project-title">
-            
+            Professional
+            <br/>
+            Experiences
         </h3>
     </div>
 </template>
