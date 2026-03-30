0fc9e0c0349d156897690b16ce0254065c09f589
Lovethrech <rachealdolapo45@gmail.com>
2026-03-30 10:41:26 +0100

    working on the home-about components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HomeHeroCtn from '@/components/home/hero/HomeHeroCtn.vue';
 import HomeAboutCtn from '@/components/home/about/HomeAboutCtn.vue';
+import HomeProjectCtn from '@/components/home/project/HomeProjectCtn.vue';
 </script>
 
 <template>
   <main id="home-view">
     <HomeHeroCtn/>
     <HomeAboutCtn/>
+    <HomeProjectCtn/>
   </main>
 </template>
