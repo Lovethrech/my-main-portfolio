c279fae2f03b22ea75a5168e69d2626b57248ed1
Lovethrech <rachealdolapo45@gmail.com>
2026-03-30 12:43:06 +0100

    working on the home-about components.

--- src/components/home/project/HomeProjectBox.vue
+++ src/components/home/project/HomeProjectBox.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="home-project-box">
         <div class="home-project-box-desc-ctn">
-            <span 
-                class="legend-dot"
-            >
-            </span>
-            <span>
-                Client
-            </span>
+            <div class="home-project-box-desc-main-ctn">
+                <span 
+                    class="legend-dot"
+                >
+                </span>
+                <span>
+                    Client
+                </span>
+            </div>
         </div>
     </div>
 </template>
@@ -15,13 +17,24 @@
 <style scoped>
 .home-project-box{
     background: url("/public/Chel-Cafe.jpeg") no-repeat;
-    background-size: contain;
+    background-size: cover;
     width:30vw;
-    height: 50vh;
+    height: 30vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    text-align: right;
+    padding: 1vh;
 }
 .home-project-box-desc-ctn{
+    background-color: red;
+    display: flex;
+    justify-content: end;
+}
+.home-project-box-desc-main-ctn{
     background-color: var(--bg-color);
-    width: 20%;
+    padding: 0.5vh 2vh;
+    border-radius: var(--border-radius);
 }
 .legend-dot {
     background-color: orange;
