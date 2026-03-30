2dfe148c0cbac96e0ec8ccd68936f543dd9ff947
Lovethrech <rachealdolapo45@gmail.com>
2026-03-30 12:28:49 +0100

    working on the home-about components.

--- src/components/home/project/HomeProjectBox.vue
+++ src/components/home/project/HomeProjectBox.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="home-project-box">
-    </div>
+    <div class="home-project-box"></div>
 </template>
 
 <style scoped>
 .home-project-box{
-    background: url()
+    background: url("/public/Adeniyi-Portfolio-Desktop.jpeg") no-repeat;
+    background-size: contain;
     width:30vh;
     height: 30vh;
 }
