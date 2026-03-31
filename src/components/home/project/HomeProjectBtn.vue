689b5d49639ed923e97b1b58e34ba4383a6937ff
Lovethrech <rachealdolapo45@gmail.com>
2026-03-31 22:28:05 +0100

    working on the home-project components.

--- src/components/home/project/HomeProjectBtn.vue
+++ src/components/home/project/HomeProjectBtn.vue
@@ -2,7 +2,7 @@
     <div class="home-project-btn-ctn display-flex-justify-center-place-center">
         <RouterLink to="/">
             <button class="home-project-btn">
-                Projects
+                PROJECTS
             </button>
         </RouterLink>
     </div>
@@ -11,11 +11,12 @@
 <style scoped>
 .home-project-btn-ctn{
     width: 100%;
-    font-weight:800;
+    
 }
 .home-project-btn{
     background-color: var(--bg-color);
     color:var(--text-color);
+    font-weight:600;
     letter-spacing: 1vh;
     padding: 1vh 3vh;
     font-size: 2vh;
