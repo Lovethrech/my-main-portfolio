66f2b6913f14b6028bd2315bb17a654216952d0a
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 21:40:24 +0100

    working on the home-contact components.

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -11,9 +11,9 @@ const contactFormStore=useContactFormStore();
 <template>
     <div class="contact-form-ctn">
         <form action="" id="contact-form" @submit="contactFormStore.submitForm">
-            <ContactInputCtn :labelName="Contacts[0].labelName" :nameType="Contacts[0].nameType" :nameFor="Contacts[0].nameFor"></ContactInputCtn>
-            <ContactInputCtn :labelName="Contacts[1].labelName" :nameType="Contacts[1].nameType" :nameFor="Contacts[1].nameFor"></ContactInputCtn>
-            <ContactInputMsgCtn :labelName="Contacts[2].labelName" :nameType="Contacts[2].nameType" :nameFor="Contacts[2].nameFor"></ContactInputMsgCtn>
+            <ContactInputCtn :labelName="Contacts[0].labelName" :nameType="Contacts[0].nameType" :nameFor="Contacts[0].nameFor" :placeholder="Contacts[0].placeholder"></ContactInputCtn>
+            <ContactInputCtn :labelName="Contacts[1].labelName" :nameType="Contacts[1].nameType" :nameFor="Contacts[1].nameFor" :placeholder="Contacts[1].placeholder"></ContactInputCtn>
+            <ContactInputMsgCtn :labelName="Contacts[2].labelName" :nameType="Contacts[2].nameType" :nameFor="Contacts[2].nameFor" :placeholder="Contacts[2].placeholder"></ContactInputMsgCtn>
             <br/>
             <ContactFormBtn @click="contactFormStore.submitForm"/>
         </form>
