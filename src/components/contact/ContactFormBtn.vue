a345e45618309bc54d646de39850371a8807a8d1
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 21:14:56 +0100

    working on the home-contact components.

--- src/components/contact/ContactFormBtn.vue
+++ src/components/contact/ContactFormBtn.vue
@@ -1,5 +1,15 @@
 <template>
-    <button type="submit">
+    <button type="submit" id="contact-form-btn">
         Submit Message
     </button>
 </template>
+
+<style scoped>
+#contact-form-btn{
+    width: 100%;
+    background-color: var(--bg-color);
+    color: var(--text-color);
+    padding: 1vh;
+    font-size: 2vh;
+}
+</style>
