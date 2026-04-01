92dd4b6c0fdcf9816a77849780d246e64a44d888
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 20:32:28 +0100

    working on the home-contact components.

--- src/stores/contactForm.js
+++ src/stores/contactForm.js
@@ -2,5 +2,5 @@ import { defineStore } from "pinia";
 import {ref, watch} from "vue";
 
 export const useContactFormStore=defineStore('contactForm', ()=>{
-    
+    const form=ref("contact-form");
 })
