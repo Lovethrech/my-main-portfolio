b77911374efd9f0b995f96cc1f8bc4ccbd4fbf3d
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 18:21:43 +0100

    working on the home-contact components.

--- src/components/contact/ContactCtn.vue
+++ src/components/contact/ContactCtn.vue
@@ -1,7 +1,11 @@
+<script setup>
+import ContactTitle from './ContactTitle.vue';
+</script>
+
 <template>
     <section class="contact-ctn">
         <div class="contact-main-ctn">
-            
+            <ContactTitle/>
         </div>
     </section>
 </template>
--- src/components/contact/ContactTitle.vue
+++ src/components/contact/ContactTitle.vue
@@ -3,3 +3,14 @@
         Contact
     </h3>
 </template>
+
+<style scoped>
+.contact-title{
+    color: #b2fffc;
+    font-family: "Orbitron", serif;
+    font-size:4vh;
+    text-transform: uppercase;
+    text-align: left;
+    font-style: normal;
+}
+</style>
