47e12f693436511a58378ee11136aeb027745944
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 18:55:30 +0100

    working on the home-contact components.

---
+++ src/components/contact/ContactDesc.vue
@@ -0,0 +1,8 @@
+<template>
+    <p class="contact-desc">
+        I would love to hear about your projects and how I could help. 
+        <br>
+        Please fill in the form, and I'll get back to you
+        as soon as possible.
+    </p>
+</template>
