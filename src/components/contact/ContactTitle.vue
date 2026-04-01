9009705e89908b4ae24c90e82012ea6201bffac2
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 18:27:15 +0100

    working on the home-contact components.

--- src/components/contact/ContactTitle.vue
+++ src/components/contact/ContactTitle.vue
@@ -1,7 +1,15 @@
 <template>
-    <h3 class="contact-title">
-        Contact
-    </h3>
+    <div class="contact-title-and-desc">
+        <h3 class="contact-title">
+            Contact
+        </h3>
+        <p class="contact-desc">
+            I would love to hear about your projects and how I could help. <br>
+            Please fill in the form, and I'll get back to you
+            as soon as possible.
+        </p>
+    </div>
+    
 </template>
 
 <style scoped>
