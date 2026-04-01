fb9590c688b2aaf81eb5d67cf7412fe01080017c
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 21:04:23 +0100

    working on the home-contact components.

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { ref } from "vue";
+import { useContactFormStore } from "@/stores/contactForm";
 import Contacts from "@/data/contacts.json";
 import ContactInputCtn from "./ContactInputCtn.vue";
 import ContactInputMsgCtn from "./ContactInputMsgCtn.vue";
+
+const contactFormStore=useContactFormStore();
 </script>
 
 <template>
     <div class="contact-form-ctn">
-        <form action="" id="contact-form">
+        <form action="" id="contact-form" @submit="contactFormStore.submitForm">
             <ContactInputCtn :labelName="Contacts[0].labelName" :nameType="Contacts[0].nameType" :nameFor="Contacts[0].nameFor"></ContactInputCtn>
             <ContactInputCtn :labelName="Contacts[1].labelName" :nameType="Contacts[1].nameType" :nameFor="Contacts[1].nameFor"></ContactInputCtn>
             <ContactInputMsgCtn :labelName="Contacts[2].labelName" :nameType="Contacts[2].nameType" :nameFor="Contacts[2].nameFor"></ContactInputMsgCtn>
