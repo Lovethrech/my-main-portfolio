ed262537d7f3b69e6d2d3e9193abba49eb21950c
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 18:44:15 +0100

    working on the home-contact components.

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -1,7 +1,14 @@
+<script setup>
+import Contacts from "@/data/contacts.json";
+</script>
+
 <template>
     <div class="contact-form-ctn">
         <form action="" id="contact-form">
-            
+            <ContactInputCtn :labelName="Contacts[0].labelName" :nameType="Contacts[0].nameType" :nameFor="Contacts[0].nameFor"></ContactInputCtn>
+            <ContactInputCtn :labelName="Contacts[1].labelName" :nameType="Contacts[1].nameType" :nameFor="Contacts[1].nameFor"></ContactInputCtn>
+            <ContactInputMsgCtn :labelName="Contacts[2].labelName" :nameType="Contacts[2].nameType" :nameFor="Contacts[2].nameFor"></ContactInputMsgCtn>
+            <br/>
         </form>
     </div>
 </template>
