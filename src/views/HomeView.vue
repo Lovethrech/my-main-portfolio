8e4586012adecb260b2f3d1cef96fe855a871723
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 18:17:00 +0100

    working on the home-contact components.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import HomeHeroCtn from '@/components/home/hero/HomeHeroCtn.vue';
 import HomeAboutCtn from '@/components/home/about/HomeAboutCtn.vue';
 import HomeProjectCtn from '@/components/home/project/HomeProjectCtn.vue';
+
 </script>
 
 <template>
