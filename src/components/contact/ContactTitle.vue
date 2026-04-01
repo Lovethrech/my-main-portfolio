6a571039348c396a2c00d4978d2dc20539724c18
Lovethrech <rachealdolapo45@gmail.com>
2026-04-01 18:57:50 +0100

    working on the home-contact components.

--- src/components/contact/ContactTitle.vue
+++ src/components/contact/ContactTitle.vue
@@ -5,14 +5,9 @@
 </template>
 
 <style scoped>
-.contact-title-and-desc{
-    width: 100%;
-    text-align: left;
-}
 .contact-title{
     font-size:4vh;
-    text-transform: uppercase;
-    
+    text-transform: uppercase;    
     font-style: normal;
 }
 </style>
