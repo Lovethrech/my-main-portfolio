566b8661a4c81279e36d8b47a8332fa354b9c646
Lovethrech <rachealdolapo45@gmail.com>
2026-04-02 18:24:46 +0100

    working on the footer components.

--- src/components/about/AboutCVBtn.vue
+++ src/components/about/AboutCVBtn.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="about-download-cv-ctn">
-        
+
+        <a href="" download="olaonipekun-dolapo-rachael-cv" class="link">
+            <button class="about-download-cv">
+                Download CV
+            </button>
+        </a>
     </div>
 </template>
