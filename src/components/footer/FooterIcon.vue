0bf5d009e5aeb797fce5ff2df55a5fb1a90383f7
Lovethrech <rachealdolapo45@gmail.com>
2026-04-02 17:29:51 +0100

    working on the footer components.

--- src/components/footer/FooterIcon.vue
+++ src/components/footer/FooterIcon.vue
@@ -9,7 +9,6 @@ const {FooterLinkItems}=defineProps(['FooterLinkItems']);
     <div class="footer-icon-ctn" v-for="FooterLinkItem in FooterLinkItems" :key="FooterLinkItem.id">
         <RouterLink :to="FooterLinkItem.iconLink">
             <div class="footer-icon" v-html="FooterLinkItem.icon">
-                
             </div>
         </RouterLink>
     </div>
@@ -17,9 +16,12 @@ const {FooterLinkItems}=defineProps(['FooterLinkItems']);
 
 <style scoped>
 .footer-icon-ctn{
-    font-size: var(--h5-size);
+    font-size: var(--h4-size);
+    background-color: var(--bg-color);
+    padding: 0.5vh 1vh;
+    border-radius: var(--border-radius);
 }
 .footer-icon{
-    color: #ffffff;
+    color: var(--text-color);
 }
 </style>
