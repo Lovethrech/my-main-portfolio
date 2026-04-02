a0e86d990d3d815750d84549c1dbb4e03e53efbd
Lovethrech <rachealdolapo45@gmail.com>
2026-04-02 17:55:35 +0100

    working on the footer components.

--- src/components/about/AboutMain.vue
+++ src/components/about/AboutMain.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import AboutTitle from './AboutTitle.vue';
 </script>
 
 <template>
     <section class="about-main">
-
+        <AboutTitle/>
     </section>
 </template>
---
+++ src/components/about/AboutTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2 class="about-title">
+        ABOUT ME
+    </h2>
+</template>
+
+<style scoped>
+#home-about-title{
+    color: var(--text-color);
+    text-align: center;
+    font-size: var(--h2-size);
+}
+</style>
