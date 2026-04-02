27156726e9ea9212da5da653f18cff9d52af636f
Lovethrech <rachealdolapo45@gmail.com>
2026-04-02 17:03:55 +0100

    working on the footer components.

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,9 @@
+<script setup>
+import ContactCtn from '@/components/contact/ContactCtn.vue';
+</script>
+
 <template>
     <main id="contact-view">
-        
+        <ContactCtn/>
     </main>
 </template>
