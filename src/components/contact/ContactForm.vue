14883abedebc7d0a65b1865d0f6a6c4d210a774c
Lovethrech <rachealdolapo45@gmail.com>
2026-04-03 10:50:10 +0100

    ...finetuning all components.

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -1,17 +1,14 @@
 <script setup>
-import { useContactFormStore } from "@/stores/contactForm";
 import Contacts from "@/data/contacts.json";
 import ContactInputCtn from "./ContactInputCtn.vue";
 import ContactInputMsgCtn from "./ContactInputMsgCtn.vue";
 import ContactFormBtn from "./ContactFormBtn.vue";
-
-const contactFormStore=useContactFormStore();
 </script>
 
 <template>
     <div class="contact-form-ctn">
-        <form action="" id="contact-form" @submit="contactFormStore.submitForm">
-            <ContactInputCtn :labelName="Contacts[0].labelName" :nameType="Contacts[0].nameType" :nameId="contactFormStore.name" :nameFor="Contacts[0].nameFor" :placeholder="Contacts[0].placeholder"></ContactInputCtn>
+        <form action="" id="contact-form" @submit="submitForm">
+            <ContactInputCtn :labelName="Contacts[0].labelName" :nameType="Contacts[0].nameType" :nameFor="Contacts[0].nameFor" :placeholder="Contacts[0].placeholder"></ContactInputCtn>
             <ContactInputCtn :labelName="Contacts[1].labelName" :nameType="Contacts[1].nameType" :nameFor="Contacts[1].nameFor" :placeholder="Contacts[1].placeholder"></ContactInputCtn>
             <ContactInputMsgCtn :labelName="Contacts[2].labelName" :nameType="Contacts[2].nameType" :nameFor="Contacts[2].nameFor" :placeholder="Contacts[2].placeholder"></ContactInputMsgCtn>
             <br/>
