98fcf9ca24fa0bcb66835b361d5c2d905bc781e3
Lovethrech <rachealdolapo45@gmail.com>
2026-04-03 10:43:37 +0100

    ...finetuning all components.

--- action.js
+++ action.js
@@ -1,35 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const body= document.body;
 
-    // Check saved preferences or system preference 
-    const savedTheme=localStorage.getItem('theme');
-    if (savedTheme ==='dark'){
-        body.classList.add('dark');
-    }
-    else if (savedTheme ==='light'){
-        body.classList.remove('dark')
-    }
-    else{
-        // Optional: Respect user's OS preference 
-        if(window.matchmedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-            body.classList.add('dark');
-        }
-    }
-
-    // Toggle on click 
-    toggleButton.addEventListener('click', ()=>{
-        if(body.classList.contains('dark')){
-            body.classList.remove('dark');
-            localStorage.setItem("theme", 'light');
-            toggleButton.textContent("Toggle Dark Mode");
-        }
-        else{
-            body.classList.add('dark');
-            localStorage.setItem("theme", 'dark');
-            toggleButton.textContent("Toggle White Mode");
-        }
-    })
-
     const form=document.getElementById('contact-form');
     const successMsg=document.getElementById('form-success');
 
