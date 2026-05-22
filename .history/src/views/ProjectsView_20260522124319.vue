ffdc6c35deb31015e34b146c9df747a4d2bb48df
Lovethrech <rachealdolapo45@gmail.com>
2026-05-22 12:43:51 +0100

    ...working on projects page.

---
+++ .history/src/views/ProjectsView_20260522124319.vue
@@ -0,0 +1,31 @@
+<script setup>
+import ProjectMain from "@/components/"
+</script>
+
+<template>
+    <main id="projects-view">
+        <section>
+            <h3 class="project-title">projects</h3>
+            <ProjectMain></ProjectMain>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+.project-title{
+    margin:  0 6vw;
+    border-bottom: 0.1vh solid hsl(166, 91%, 29%);    
+    color: hsl(166, 91%, 49%);
+    margin-bottom: 20px;
+    font-size:4vh;
+    text-transform: uppercase;
+    text-align:center;
+    letter-spacing:2vh;
+}
+
+@media screen and (max-width: 650px) {
+    .project-title{
+        font-size: 3vh;
+    }
+}
+</style>
